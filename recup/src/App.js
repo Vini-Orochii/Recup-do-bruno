7a3d76ea5078161764acfc58893524efc8f4de8c
Vini-Orochii <viniventura655@gmail.com>
2022-06-27 19:08:17 -0300

    recup_Bruno

---
+++ recup/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Form">
+   <input type="text" placeholder="Nome"></input>
+   <br></br>
+   <input type="text" placeholder="Telefone"></input>
+   <br></br>
+   <button>Enviar</button>
+    </div>
+  );
+}
+
+export default App;
